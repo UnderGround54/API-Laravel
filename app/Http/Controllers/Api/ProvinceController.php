3c4748825c46cea88816fff840a1f9722ff650a4
UnderGround54 <martinpros54@gmail.com>
2022-07-12 20:08:08 +0300

    Creer crud sur controller

--- app/Http/Controllers/Api/ProvinceController.php
+++ app/Http/Controllers/Api/ProvinceController.php
@@ -7,5 +7,76 @@ use Illuminate\Http\Request;
 
 class ProvinceController extends Controller
 {
+<<<<<<< Updated upstream
     //
+=======
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Province::orderByDesc('id')->get();
+
+        // return $provinces->toJson(JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (Province::create($request->all())) {
+            return response()->json([
+                'Success' => 'insertion avec success'
+            ], 200);
+       };
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Province $province)
+    {
+        return $province;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Province $province)
+    {
+        if ($province->update($request->all())) {
+            return response()->json([
+                'success' => 'Update avec success'
+            ], 200);
+        };
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Province $province)
+    {
+        if ($province->delete()){
+            return response()->json([
+                'success' => 'suppression avec succes'
+            ], 200);
+        };
+    }
+>>>>>>> Stashed changes
 }
--- app/Models/Province.php
+++ app/Models/Province.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Province extends Model
 {
     use HasFactory;
+<<<<<<< Updated upstream
+=======
+    protected $table = 'provinces';
+    protected $fillable = [
+        'libelle_prov',
+        'code_prov',
+    ];
+    public $timestamps = false;
+>>>>>>> Stashed changes
 }
